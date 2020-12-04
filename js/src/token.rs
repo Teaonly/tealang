2175342cda09f5111e41bc0ee9affe5d3793305b
Zhou Chang <achang.zhou@gmail.com>
2020-12-04 11:04:13 +0800

    clean code for js engine in rust

--- js/src/token.rs
+++ js/src/token.rs
@@ -2,15 +2,8 @@ use std::char;
 
 /* token stuff */
 #[allow(non_camel_case_types)]
-#[derive(Clone, Debug)]
-enum TokenType {
-	/* general token */
-    TK_EOF_ = -100,
-	TK_COMMENT_,
-    TK_SYMBOL_,
-    TK_STRING_,
-    TK_PUNCT_,
-
+#[derive(Clone, Debug, PartialEq)]
+pub enum TokenType {
 	/* immedial primitive */
 	TK_IDENTIFIER = 0,
     TK_NUMBER,
@@ -87,9 +80,10 @@ enum TokenType {
 	TK_WITH
 }
 
-struct Token {
-    tk_type:     TokenType,
-    tk_value:    Option<String>,
+#[derive(Clone, Debug)]
+pub struct Token {
+    pub tk_type:     TokenType,
+    pub tk_value:    Option<String>,
 }
 
 impl Token {
@@ -108,9 +102,42 @@ impl Token {
     }
 }
 
+#[allow(non_camel_case_types)]
+#[derive(Clone, Debug, PartialEq)]
+enum GeneralTokenType {
+    TK_EOF_,
+	TK_COMMENT_,
+    TK_SYMBOL_,
+    TK_STRING_,
+    TK_PUNCT_,
+}
+
+#[derive(Clone, Debug)]
+struct GeneralToken {
+    pub tk_type:     GeneralTokenType,
+    pub tk_value:    Option<String>,
+}
+
+impl GeneralToken {
+    fn new(tt: GeneralTokenType) -> Self {
+        GeneralToken {
+            tk_type: tt,
+            tk_value: None
+        }
+    }
+
+    fn new_with(tt: GeneralTokenType, value: String) -> Self {
+        GeneralToken {
+            tk_type: tt,
+            tk_value: Some(value)
+        }
+    }
+}
+
+
 /// Parsing token from code string, return next token.
 ///
-fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize), &'static str> {
+fn next_general_token (script: &str, cursor: usize) -> Result<(GeneralToken, usize), &'static str> {
     // local helper
 	#[allow(non_camel_case_types)]
 	enum ct {
@@ -145,8 +172,8 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
         if ch == '\n' {
             return ct::CT_NEWLN;
         }
-        if ch == '.' || ch == '-' || ch == '+' || ch == '*' || ch == '/' || ch == '%' || ch == '=' || ch == ';' || ch == '\\'
-            || ch == '&' || ch == '!' || ch == '|' || ch == '^' || ch == '!' || ch == '\'' || ch == '"' || ch == '\n'
+        if ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '=' || ch == ';' || ch == '\\'
+            || ch == '&' || ch == '!' || ch == '|' || ch == '^' || ch == ',' || ch == '\'' || ch == '"'
             || ch == '<' || ch == '>' || ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}' {
             return ct::CT_PUNCT;
         }
@@ -163,9 +190,29 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
         return c;
     }
 
+    const VALID_PUNCTS:  [&'static str; 24] =
+        [ "<=", ">=", "==", "!=", "===", "!==",
+          "<<", ">>", ">>>", "&&", "||",
+          "+=", "-=", "*=", "/=", "%=",
+          "<<=", ">>=", ">>>=", "&=", "|=", "^=",
+          "++", "--"];
+
+    fn check_punct(value: &String) -> bool {
+        for i in 0..VALID_PUNCTS.len() {
+            if VALID_PUNCTS[i] == value {
+                return true;
+            }
+        }
+        return false;
+    }
+
     //
     // main code starting here
     //
+    if cursor >= script.len() {
+        let eof = GeneralToken::new(GeneralTokenType::TK_EOF_);
+        return Ok((eof, cursor));
+    }
     let code = &script[cursor..];
 	let mut chars = code.chars();
 	let mut pos = cursor;
@@ -183,14 +230,14 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
         if ps == ps::PS_NULL {
             match ct {
                 ct::CT_EOF => {
-                    let eof = Token::new(TokenType::TK_EOF_);
+                    let eof = GeneralToken::new(GeneralTokenType::TK_EOF_);
                     return Ok((eof, pos));
                 },
                 ct::CT_SPACE => {
                     continue;
                 },
                 ct::CT_NEWLN => {
-                    let ln = Token::new_with(TokenType::TK_PUNCT_, String::from("\n"));
+                    let ln = GeneralToken::new_with(GeneralTokenType::TK_PUNCT_, String::from("\n"));
                     return Ok((ln, pos));
                 },
                 ct::CT_LETTER => {
@@ -208,12 +255,15 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
                         ps = ps::PS_STRING_DOUBLE;
                         continue;
                     }
+                    if ch == ';' {
+                        let punct = GeneralToken::new_with(GeneralTokenType::TK_PUNCT_, String::from(";"));
+                        return Ok((punct, pos));
+                    }
                     tkbuf.push(ch);
                     ps = ps::PS_PUNCT;
                     continue;
                 },
             }
-            panic!("FIXME, Can't run here!");
         }
 
         // state handler
@@ -221,12 +271,12 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
             match ct {
                 ct::CT_EOF | ct::CT_SPACE => {
                     let value = tkbuf.into_iter().collect();
-                    let symbol = Token::new_with(TokenType::TK_SYMBOL_, value);
+                    let symbol = GeneralToken::new_with(GeneralTokenType::TK_SYMBOL_, value);
                     return Ok((symbol, pos));
                 },
                 ct::CT_NEWLN => {
                     let value = tkbuf.into_iter().collect();
-                    let symbol = Token::new_with(TokenType::TK_SYMBOL_, value);
+                    let symbol = GeneralToken::new_with(GeneralTokenType::TK_SYMBOL_, value);
                     return Ok((symbol, pos - 1));
                 },
                 ct::CT_LETTER => {
@@ -235,11 +285,10 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
                 },
                 ct::CT_PUNCT => {
                     let value = tkbuf.into_iter().collect();
-                    let symbol = Token::new_with(TokenType::TK_SYMBOL_, value);
+                    let symbol = GeneralToken::new_with(GeneralTokenType::TK_SYMBOL_, value);
                     return Ok((symbol, pos-1));
                 }
             }
-            panic!("FIXME, Can't run here!");
         }
 
         // state handler
@@ -261,12 +310,12 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
                     }
                     if ch == '\'' && ps == ps::PS_STRING_SINGLE {
                         let value = tkbuf.into_iter().collect();
-                        let string = Token::new_with(TokenType::TK_STRING_, value);
+                        let string = GeneralToken::new_with(GeneralTokenType::TK_STRING_, value);
                         return Ok((string, pos));
                     }
                     if ch == '"' && ps == ps::PS_STRING_DOUBLE {
                         let value = tkbuf.into_iter().collect();
-                        let string = Token::new_with(TokenType::TK_STRING_, value);
+                        let string = GeneralToken::new_with(GeneralTokenType::TK_STRING_, value);
                         return Ok((string, pos));
                     }
                     if ch == '\\' {
@@ -277,7 +326,6 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
                     continue;
                 }
             }
-            panic!("FIXME, Can't run here!");
         }
 
         // state handler
@@ -293,13 +341,12 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
                         tkbuf.pop();
                         tkbuf.pop();
                         let value = tkbuf.into_iter().collect();
-                        let comment = Token::new_with(TokenType::TK_COMMENT_, value);
+                        let comment = GeneralToken::new_with(GeneralTokenType::TK_COMMENT_, value);
                         return Ok((comment, pos));
                     }
                     continue;
                 },
             }
-            panic!("FIXME, Can't run here!");
         }
 
         // state handler
@@ -307,7 +354,7 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
             match ct {
                 ct::CT_EOF | ct::CT_NEWLN => {
                     let value = tkbuf.into_iter().collect();
-                    let comment = Token::new_with(TokenType::TK_COMMENT_, value);
+                    let comment = GeneralToken::new_with(GeneralTokenType::TK_COMMENT_, value);
                     return Ok((comment, pos));
                 },
                 ct::CT_PUNCT | ct::CT_LETTER | ct::CT_SPACE => {
@@ -315,11 +362,119 @@ fn next_general_token (script: &String, cursor: usize) -> Result<(Token, usize),
                     continue;
                 },
             }
-            panic!("FIXME, Can't run here!");
         }
 
         // state handler
-
+        if ps == ps::PS_PUNCT {
+            match ct {
+                ct::CT_EOF | ct::CT_NEWLN | ct::CT_SPACE => {
+                    let value = tkbuf.into_iter().collect();
+                    let punct = GeneralToken::new_with(GeneralTokenType::TK_PUNCT_, value);
+                    return Ok((punct, pos));
+                },
+                ct::CT_LETTER => {
+                    let value = tkbuf.into_iter().collect();
+                    let punct = GeneralToken::new_with(GeneralTokenType::TK_PUNCT_, value);
+                    return Ok((punct, pos-1));
+                },
+                ct::CT_PUNCT => {
+                    let ch = chr.unwrap();
+                    if ch == ';' {
+                        let value = tkbuf.into_iter().collect();
+                        let punct = GeneralToken::new_with(GeneralTokenType::TK_PUNCT_, value);
+                        return Ok((punct, pos-1));
+                    }
+                    {
+                        // check is valid multiple punctuators
+                        let mut value = String::new();
+                        for i in 0..tkbuf.len() {
+                            value.push(tkbuf[i]);
+                        }
+                        value.push(ch);
+                        if value == "//" {
+                            tkbuf.clear();
+                            ps = ps::PS_COMMENT_LINE;
+                            continue;
+                        }
+                        if value == "/*" {
+                            tkbuf.clear();
+                            ps = ps::PS_COMMENT_LINE;
+                            continue;
+                        }
+                        if check_punct(&value) == true {
+                            tkbuf.push(ch);
+                            continue;
+                        }
+                    }
+                    let value = tkbuf.into_iter().collect();
+                    let punct = GeneralToken::new_with(GeneralTokenType::TK_PUNCT_, value);
+                    return Ok((punct, pos-1));
+                }
+            }
+        }
     }
-    return Err("Hello");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let script = r#"
+            // program to check if a number is prime or not
+
+            // take input from the user
+            const number = parseInt(prompt("Enter a positive number: "));
+            let isPrime = true;
+
+            if ( number >= 1.342E+3+45.0 ) {
+                console.log("number is too bigger");
+            }
+
+            // check if number is equal to 1
+            if (number === 1) {
+                console.log("1 is neither prime nor composite number.");
+            }
+
+            // check if number is greater than 1
+            else if (number > 1) {
+
+                // looping through 2 to number-1
+                for (let i = 2; i < number; i++) {
+                    if (number % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) {
+                    console.log(`${number} is a prime number`);
+                } else {
+                    console.log(`${number} is a not prime number`);
+                }
+            }
+
+            // check if number is less than 1
+            else {
+                console.log("The number is not a prime number.");
+            }
+        "#;
+
+        let mut cursor = 0;
+        loop {
+            let result = next_general_token(&script, cursor);
+            if let Ok((tk, pos)) = result {
+                if tk.tk_type == GeneralTokenType::TK_EOF_ {
+                    break;
+                }
+                println!(">>>{} : {:?}", pos, tk);
+                cursor = pos;
+                continue;
+            }
+            println!(">>>*{:?}", result);
+            break;
+        }
+    }
+}
+
