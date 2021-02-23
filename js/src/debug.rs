5805ee693c707d5fafabafb73678b7f8657e1de2
Zhou Chang <achang.zhou@gmail.com>
2021-02-23 18:25:09 +0800

    clean code

--- js/src/debug.rs
+++ js/src/debug.rs
@@ -16,8 +16,72 @@ use compile::*;
 use runtime::*;
 
 static script: &str = r#"    
-    var a = 1;
-    println(a);
+    var r, a;
+    r = 1 + 2;
+    assert(r == 3, "1 + 2 === 3");
+
+    r = 1 - 2;
+    assert(r == -1, "1 - 2 === -1");
+
+    r = -1;
+    assert(r == -1, "-1 === -1");
+
+    r = +2;
+    assert(r == 2, "+2 === 2");
+
+    r = 2 * 3;
+    assert(r == 6, "2 * 3 === 6");
+
+    r = 4 / 2;
+    assert(r == 2, "4 / 2 === 2");
+
+    r = 4 % 3;
+    assert(r == 1, "4 % 3 === 3");
+
+    r = 4 << 2;
+    assert(r == 16, "4 << 2 === 16");
+
+    r = 1 << 0;
+    assert(r == 1, "1 << 0 === 1");
+
+    r = 1 << 31;
+    assert(r == 2147483648, "1 << 31 === 2147483648");
+
+    r = 1 << 32;
+    assert(r == 1, "1 << 32 === 1");
+
+    r = (1 << 31) > 0;
+    assert(r == true, "(1 << 31) > 0 === true");
+
+    r = -4 >> 1;
+    assert(r == -2, "-4 >> 1 === -2");
+
+    // FIXME 
+    //r = -4 >>> 1;
+    //assert(r == 0x7ffffffe, "-4 >>> 1 === 0x7ffffffe");
+
+    r = 1 & 1;
+    assert(r == 1, "1 & 1 === 1");
+
+    r = 0 | 1;
+    assert(r == 1, "0 | 1 === 1");
+
+    r = 1 ^ 1;
+    assert(r == 0, "1 ^ 1 === 0");
+
+    r = ~1;
+    assert(r == -2, "~1 === -2");
+
+    r = !1;
+    assert(r == false, "!1 === false");
+
+    assert((1 < 2) == true, "(1 < 2) === true");
+
+    assert((2 > 1) == true, "(2 > 1) === true");
+
+    assert(('b' > 'a') == true, "('b' > 'a') === true");
+
+    println("-------- END TESTING -----------");
 "#;
 
 fn debug_runtime() {
