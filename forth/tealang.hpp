49db7c06271141338b573a6fe577f2a21cb9addb
Zhou Chang <achang.zhou@gmail.com>
2022-02-15 10:55:00 +0800

    clean code

--- forth/tealang.hpp
+++ forth/tealang.hpp
@@ -16,9 +16,23 @@
 namespace tea {
 
 using SPFLOAT = float;
-using SPEXTEND = std::weak_ptr<int>;
+using SPEXTEND = std::shared_ptr<int>;
 
+// forward declare
 struct CellStack;
+struct TeaLang;
+
+struct NativeWord;
+struct UserWord;
+struct SyntaxElement;
+
+using UserCode = std::vector<SyntaxElement>;        // uncompiled
+using UserBinary = std::vector<SyntaxElement>;      // compiled
+
+using nword_creator_t = std::shared_ptr<NativeWord> (*) (TeaLang&);
+using nword_t = std::shared_ptr<NativeWord>;
+using uword_t = std::shared_ptr<UserWord>;
+
 struct Cell {
     enum CellType {
         T_Number = -3,
@@ -26,14 +40,13 @@ struct Cell {
         T_Extend = -1,
         T_Tuple = 0,
     };
-
     const int type_;
     union {
         SPFLOAT           _float;
         const SPFLOAT*    _tuple;
         const char*       _str;
+        SPEXTEND*         _ext;
     } v;
-    SPEXTEND              ext_;
 
     Cell() : type_(T_Number) {
         v._float = 0.0;
@@ -41,8 +54,8 @@ struct Cell {
     Cell(SPFLOAT value): type_(T_Number) {
         v._float = value;
     }
-    Cell(SPEXTEND ext): type_(T_Extend) {
-        ext_ = ext;
+    Cell(SPEXTEND* ext): type_(T_Extend) {
+        v._ext = ext;
     }
 
 private:
@@ -52,8 +65,118 @@ private:
     Cell(const char* str): type_(T_String) {
         v._str = str;
     }
+        friend struct CellStack;
+};
 
-    friend struct CellStack;
+struct UserHash {
+public:
+    UserHash(std::map<std::string, Cell>* g, std::map<std::string, Cell>* l) {
+        global_ = p;
+        local_ = l;
+    }
+    void set(const std::string& name, Cell item) {
+        if ( local_->find(name) != local_->end() ) {
+            tt_panic("Can't re-write hash item");
+        }
+        (*local_)[name] = item;
+    }
+    Cell get(const std::string& name) {
+        if ( local_->find(name) == local_->end() ) {
+            if (global_ != NULL) {
+                if ( global_->find(name) == global_->end()) {
+                    tt_panic("Can't find hash item in local and global");
+                }
+                return (*global_)[name];
+            } else {
+                tt_panic("Can't find hash item in global");
+            }
+        }
+        return (*local_)[name];
+    }
+
+private:
+    std::map<std::string, Cell>* global_;
+    std::map<std::string, Cell>* local_;
+};
+
+// Compound type, used by Code(uncompiled Symbol) and Word(Compiled Word)
+// txt -> tokens (Uncompiled) -> SyntaxElement
+struct SyntaxElement {
+    enum SyntaxType {
+        // uncompiled value,
+        T_Number,
+        T_String,
+        T_Tuple,
+        T_NativeSymbol,
+        T_UserSymbol,
+
+        // compiled
+        T_Cell,             // String | Tuple | Number
+        T_NativeWord,
+        T_UserWord,
+    };
+    SyntaxType type_;
+
+    SPFLOAT v_float;
+    std::string v_str;
+    std::vector<float> v_tuple;
+
+    Cell v_cell;
+    nword_t v_nword;
+    uword_t v_uword;
+};
+
+// buit-in word implement in c++ code
+struct NativeWord {
+    virtual void boot(TeaLang& rt, UserHash& hash) = 0;
+    virtual void run(TeaLang& rt, UserHash& hash) = 0;
+};
+
+struct UserWord {
+    UserWord(UserBinary& binary): binary_(binary) {}
+    void boot(TeaLang& rt, std::map<std::string, Cell>* global_hash_) {
+        local_hash_.clear();
+
+        UserHash uhash(global_hash_, &local_hash_);
+
+        for (size_t i = 0; i < binary_.size(); i++) {
+            if ( binary_[i].type_ == SyntaxElement::T_Cell ) {
+                rt.stack().push_cell( binary_[i].v_cell );
+                continue;
+            }
+            if ( binary_[i].type_ == SyntaxElement::T_NativeWord ) {
+                binary_[i].v_nword->boot(rt, uhash);
+                continue;
+            }
+            if ( binary_[i].type_ == SyntaxElement::T_UserWord ) {
+                binary_[i].v_uword->boot(rt, global_hash_);
+                continue;
+            }
+            tt_panic("FIXME: Can't be here");
+        }
+    }
+    void run(TeaLang& rt) {
+        for (size_t i = 0; i < binary_.size(); i++) {
+            switch( binary_[i].type_ ) {
+                case SyntaxElement::T_Cell:
+                    if ( binary_[i]
+                    break;
+                case SyntaxElement::T_NativeWord:
+                    binary_[i].v_nword->run(rt);
+                    break;
+                case SyntaxElement::T_UserWord:
+                    binary_[i].v_uword->run(rt);
+                    break;
+                default:
+                    tt_panic("FIXME: Can't be here");
+                    break;
+            }
+        }
+
+    }
+private:
+    const UserBinary binary_;
+    std::map<std::string, Cell> local_hash_;
 };
 
 struct CellStack {
@@ -67,6 +190,26 @@ public:
             delete all_tuple_[i];
         }
     }
+
+    virtual void push_float(SPFLOAT v) = 0;
+    virtual SPFLOAT back_float() = 0;
+    virtual SPFLOAT pop_float() = 0;
+    virtual const char* pop_string() = 0;
+    virtual const SPFLOAT* pop_tuple() = 0;
+    virtual void push_extend(SPEXTEND* ext) = 0;
+    virtual SPEXTEND* pop_extend() = 0;
+    virtual void push_cell(Cell& c) = 0;;
+    virtual Cell pop_cell() = 0;
+
+    virtual void drop() = 0;
+    virtual void dup() = 0;
+    virtual void dup2() = 0;
+    virtual void swap() = 0;
+    virtual void rot() = 0;
+    virtual size_t size() = 0;
+    virtual void clear() = 0;
+
+private:
     Cell new_tuple( std::vector<SPFLOAT>& vec) {
         SPFLOAT* t = new SPFLOAT[ vec.size() ];
 
@@ -84,24 +227,6 @@ public:
 
         return Cell(local_str);
     }
-
-    virtual void push_float(SPFLOAT v) = 0;
-    virtual SPFLOAT back_float() = 0;
-    virtual SPFLOAT pop_float() = 0;
-    virtual const char* pop_string() = 0;
-    virtual const SPFLOAT* pop_tuple();
-    virtual void push_extend(SPEXTEND ext) = 0;
-    virtual SPEXTEND pop_extend() = 0;
-    virtual void push_cell(Cell& c);
-    virtual Cell pop_cell();
-
-    virtual void drop() = 0;
-    virtual void dup() = 0;
-    virtual void dup2() = 0;
-    virtual void swap() = 0;
-    virtual void rot() = 0;
-    virtual size_t size() = 0;
-    virtual void clear() = 0;
 
 private:
     std::vector<const char*> all_string_;
@@ -158,10 +283,6 @@ public:
         tt_assert(c.type_ >= Cell::T_Tuple, "cell is not tuple!");
         return c.v._tuple;
     }
-    virtual void push_extend(SPEXTEND ext) {
-        Cell c(ext);
-        stack_.push_back(c);
-    }
     virtual SPEXTEND pop_extend() {
         if (stack_.size() == 0) {
             tt_panic("Can't pop from empty stack!");
@@ -169,7 +290,7 @@ public:
         auto c = stack_.back();
         stack_.pop_back();
         tt_assert(c.type_ == Cell::T_Extend, "cell is not extend!");
-        return c.ext_;
+        return *c.v._ext;
     }
     virtual void push_cell(Cell& c) {
         stack_.push_back(c);
@@ -229,6 +350,10 @@ public:
     }
 
 private:
+    // Native and User dictionary
+    std::map<std::string, nword_creator_t> ndict_;
+    std::map<std::string, UserCode> udict_;
+
     std::vector<Cell> stack_;
 };
 
